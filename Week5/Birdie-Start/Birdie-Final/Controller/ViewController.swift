4e4f11e92a15d75ad05268f67486ba7519b5d1dc
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2020-07-11 00:42:48 +0200

    Added alert with texfields on tap on create text post button

--- Week5/Birdie-Start/Birdie-Final/Controller/ViewController.swift
+++ Week5/Birdie-Start/Birdie-Final/Controller/ViewController.swift
@@ -12,13 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableview: UITableView!
 
-    var posts = [MediaPost]()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTableView()
         MediaPostsHandler.shared.getPosts()
-        posts = MediaPostsHandler.shared.mediaPosts
     }
 
     func setUpTableView() {
@@ -31,21 +28,52 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didPressCreateTextPostButton(_ sender: Any) {
-
+       displayTextPostAlert()
     }
 
     @IBAction func didPressCreateImagePostButton(_ sender: Any) {
 
     }
+
+    private func displayTextPostAlert() {
+        let alert = UIAlertController(title: "Add new post", message: "", preferredStyle: .alert)
+
+        alert.addTextField { (userTextfield) in
+            userTextfield.placeholder = "Username"
+        }
+        alert.addTextField { (textTextfield) in
+            textTextfield.placeholder = "Text"
+        }
+
+        let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+            if let textfields = alert.textFields {
+                if !textfields[0].text!.isEmpty && !textfields[1].text!.isEmpty {
+                    let post = TextPost(textBody: textfields[1].text,
+                                        userName: textfields[0].text!,
+                                        timestamp: Date())
+                    MediaPostsHandler.shared.addTextPost(textPost: post)
+                    self.tableview.reloadData()
+                } else {
+                    print("You need to fill all the fields")
+                }
+            }
+        }
+                
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+        return MediaPostsHandler.shared.mediaPosts.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let post = posts[indexPath.row]
+        let post = MediaPostsHandler.shared.mediaPosts[indexPath.row]
 
         if post is TextPost {
             guard let cell = tableview.dequeueReusableCell(withIdentifier: CustomTextCell.identifier, for: indexPath) as? CustomTextCell else { return UITableViewCell() }
