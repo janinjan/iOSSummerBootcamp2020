e4821a52117c959ec710e40ee75f2b2f8c739f0a
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2020-07-10 19:03:02 +0200

    Created xibs for custom cells: One for text message, one for text and image message

---
+++ Week5/Birdie-Start/Birdie-Final/View/CustomImageAndTextCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomImageAndTextCell.swift
+//  Birdie-Final
+//
+//  Created by Janin Culhaoglu on 10/07/2020.
+//  Copyright © 2020 Jay Strawn. All rights reserved.
+//
+
+import UIKit
+
+class CustomImageAndTextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Week5/Birdie-Start/Birdie-Final/View/CustomTextCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTextCell.swift
+//  Birdie-Final
+//
+//  Created by Janin Culhaoglu on 10/07/2020.
+//  Copyright © 2020 Jay Strawn. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
