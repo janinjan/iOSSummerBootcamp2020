7cbae0e26cc24c8f0ebb3493a21fcc0eaa00fb46
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2020-08-09 12:07:46 +0200

    Added a menu button with animation

--- Week11/AnimateMenu/AnimateMenu/ViewController.swift
+++ Week11/AnimateMenu/AnimateMenu/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet private var menuButton: UIButton!
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var circleView: UIView!
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+
+    // MARK: - Properties
+    private var isMenuOpen = false
+    var buttonOneCenter: CGPoint!
+    var buttonTwoCenter: CGPoint!
+    var buttonThreeCenter: CGPoint!
+    var circleViewCenter: CGPoint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        assignbackground()
+        circleView.layer.cornerRadius = circleView.frame.width/2
+        buttonOneCenter = buttonOne.center
+        buttonTwoCenter = buttonTwo.center
+        buttonThreeCenter = buttonThree.center
+
+        buttonOne.center = menuButton.center
+        buttonTwo.center = menuButton.center
+        buttonThree.center = menuButton.center
+
+        circleView.layer.zPosition = -1
     }
 
+    // MARK: - Actions
+    @IBAction func toggleMenu(_ sender: UIButton) {
+        isMenuOpen.toggle()
 
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .allowUserInteraction, animations: {
+            self.buttonOne.center =  self.isMenuOpen ? self.buttonOneCenter : self.menuButton.center
+            self.buttonTwo.center = self.isMenuOpen ? self.buttonTwoCenter : self.menuButton.center
+            self.buttonThree.center = self.isMenuOpen ? self.buttonThreeCenter : self.menuButton.center
+
+            self.buttonOne.alpha = self.isMenuOpen ? 1 : 0
+            self.buttonTwo.alpha = self.isMenuOpen ? 1 : 0
+            self.buttonThree.alpha = self.isMenuOpen ? 1 : 0
+            self.circleView.alpha = self.isMenuOpen ? 0.5 : 0
+
+            self.menuButton.transform = .init(scaleX: self.isMenuOpen ? 0.8 : 1, y: self.isMenuOpen ? 0.8 : 1)
+            self.circleView.transform = .init(scaleX: self.isMenuOpen ? 1 : 0.1, y: self.isMenuOpen ? 1 : 0.1)
+            self.menuButton.setBackgroundImage(UIImage(systemName: self.isMenuOpen ? "play.circle.fill" : "plus.circle.fill"), for: .normal)
+        })
+    }
+
+    private func assignbackground() {
+        let background = UIImage(named: "violetRectangle")
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
 }
-
